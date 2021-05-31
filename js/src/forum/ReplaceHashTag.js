e225b5a6dc845531c8f4a29da1ffce00a19f6625
Justoverclock <info@justoverclock.it>
2021-05-31 15:01:24 +0200

    New Regex rule

--- js/src/forum/ReplaceHashTag.js
+++ js/src/forum/ReplaceHashTag.js
@@ -2,10 +2,11 @@ import app from 'flarum/app';
 
 export default function () {
 
-  const regex = /((?!([\S]))[\S\s])#(\p{L}+)/gu;
+  const regex = /#[^\s!@#$%^&*()=+.\/,\[{\]};:'"?><]+/g;
+
   const p = this.$('.Post-body');
   const baseurl = app.forum.attribute('baseUrl');
 
-  p.html = p.html(p.html().replace(regex, match => `<a href="${baseurl}/?q=$3" class="hashlink" title="Find more post with this hashtag">${match}</a>`))
+  p.html = p.html(p.html().replace(regex, match => `<a href="${baseurl}/?q=${match}" class="hashlink" title="Find more post with this hashtag">${match}</a>`))
 
 }
