f3dec548f5e51787fa4e57acbc9c188cb7d0e0f8
Marco Colia <info@justoverclock.it>
2021-06-01 19:01:15 +0200

    regexp updated

--- js/src/forum/ReplaceHashTag.js
+++ js/src/forum/ReplaceHashTag.js
@@ -1,13 +1,13 @@
 import app from 'flarum/app';
 
 export default function () {
-  const regex = /<\/?\w+((\s+\w+(\s*=\s*(?:\".*?"|'.*?'|[^'\">\s]+))?)+\s*|\s*)\/?>#[^\s!@#$%^&*()=+.\/,\[{\]};:'"?><]+/g;
 
+  const regex = /(?<!html)#[^\s!@#$%^&*()=+.\/,\[{\]};:'"?><]+/g
   const p = this.$('.Post-body');
   const baseurl = app.forum.attribute('baseUrl');
 
   //rimuoviamo il carattere # utilizzando match.slice nel link
   p.html = p.html(
-    p.html().replace(regex, (match) => `<a href="${baseurl}/?q=${match.slice(4)}" class="hasht" title="Search this hashtag into Flarum">${match}</a>`)
+    p.html().replace(regex, (match) => `<a href="${baseurl}/?q=${match.slice(1)}" class="hasht" title="Search this hashtag into Flarum">${match}</a>`)
   );
 }
