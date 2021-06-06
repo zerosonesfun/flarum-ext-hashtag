dda5711b86ed27c5c015b5af9b9c6bedaef82bfa
Justoverclock <79002016+justoverclockl@users.noreply.github.com>
2021-06-06 18:41:52 +0200

    Fixed composer error

--- js/src/forum/ReplaceHashTag.js
+++ js/src/forum/ReplaceHashTag.js
@@ -2,12 +2,15 @@ import app from 'flarum/app';
 
 export default function () {
 
-  const regex = /(?<=\s|^)#(\w*[A-Za-z_]+\w*)/g;
+  const regex = /#(\w*[0-9a-zA-Z]+\w*[0-9a-zA-Z])/g;
   const p = this.$('p');
   const baseurl = app.forum.attribute('baseUrl');
 
-  //rimuoviamo il carattere # utilizzando match.slice nel link
-  p.html = p.html(
-    p.html().replace(regex, (match) => `<a href="${baseurl}/?q=${match.slice(1)}" class="hasht" title="Search this hashtag into Flarum">${match}</a>`)
-  );
+  // rimuoviamo il carattere # utilizzando match.slice nel link
+  // thanks to Nearata for this fix
+  p.each((index, element) => {
+    $(element).html(
+      $(element).html().replace(regex, (match) => `<a href="${baseurl}/?q=${match.slice(1)}" class="hasht" title="Search this hashtag into Flarum">${match}</a>`)
+    );
+  });
 }
