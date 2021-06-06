ea5571827cd2b0b5a667e0035ef896e2c2861828
Justoverclock <79002016+justoverclockl@users.noreply.github.com>
2021-06-06 18:51:40 +0200

    fixed regex

--- js/src/forum/ReplaceHashTag.js
+++ js/src/forum/ReplaceHashTag.js
@@ -1,8 +1,7 @@
 import app from 'flarum/app';
 
 export default function () {
-
-  const regex = /#(\w*[0-9a-zA-Z]+\w*[0-9a-zA-Z])/g;
+  const regex = /(?<=\s|^)#(\w*[A-Za-z_]+\w*)/g;
   const p = this.$('p');
   const baseurl = app.forum.attribute('baseUrl');
 
@@ -10,7 +9,9 @@ export default function () {
   // thanks to Nearata for this fix
   p.each((index, element) => {
     $(element).html(
-      $(element).html().replace(regex, (match) => `<a href="${baseurl}/?q=${match.slice(1)}" class="hasht" title="Search this hashtag into Flarum">${match}</a>`)
+      $(element)
+        .html()
+        .replace(regex, (match) => `<a href="${baseurl}/?q=${match.slice(1)}" class="hasht" title="Search this hashtag into Flarum">${match}</a>`)
     );
   });
 }
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,7 +3,6 @@ import { extend } from 'flarum/common/extend';
 import CommentPost from 'flarum/components/CommentPost';
 import ReplaceHashTag from './ReplaceHashTag';
 
-
 app.initializers.add('justoverclock/flarum-ext-hashtag', () => {
   extend(CommentPost.prototype, ['oncreate', 'onupdate'], ReplaceHashTag);
 });
