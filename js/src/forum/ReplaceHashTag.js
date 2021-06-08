2ffd20c6c056a128a675d088e11ae598ae65be51
iPurpl3x <rafael.horvat@icloud.com>
2021-06-08 08:24:49 +0200

    translated hashtag link tooltip

--- js/src/forum/ReplaceHashTag.js
+++ js/src/forum/ReplaceHashTag.js
@@ -4,6 +4,7 @@ export default function () {
   const regex = /(?<=\s|^)#(\w*[A-Za-z_]+\w*)/g;
   const p = this.$('p');
   const discussionsUrl = app.route('index');
+  const tooltip = app.translator.trans('justoverclock-hashtag.forum.post.hashtag_link_tooltip');
 
   // rimuoviamo il carattere # utilizzando match.slice nel link
   // thanks to Nearata for this fix
@@ -11,10 +12,7 @@ export default function () {
     $(element).html(
       $(element)
         .html()
-        .replace(
-          regex,
-          (match) => `<a href="${discussionsUrl}/?q=${match.slice(1)}" class="hasht" title="Search this hashtag into Flarum">${match}</a>`
-        )
+        .replace(regex, (match) => `<a href="${discussionsUrl}/?q=${match.slice(1)}" class="hasht" title="${tooltip}">${match}</a>`)
     );
   });
 }
