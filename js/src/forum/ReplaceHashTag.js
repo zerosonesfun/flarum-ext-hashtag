e5fdd1798095c5363cfec3cd91aa051a533dd9f4
Justoverclock <info@justoverclock.it>
2021-06-12 20:00:33 +0200

    Safari compatible Fix

--- js/src/forum/ReplaceHashTag.js
+++ js/src/forum/ReplaceHashTag.js
@@ -1,7 +1,8 @@
 import app from 'flarum/forum/app';
 
 export default function () {
-  const regex = /(?<=\s|^)#(\w*[A-Za-z_]+\w*)/g;
+
+  const regex = /(^|\s)#(\p{L}+)/gu;
   const p = this.$('p');
   const discussionsUrl = app.route('index');
   const tooltip = app.translator.trans('justoverclock-hashtag.forum.post.hashtag_link_tooltip');
@@ -12,7 +13,7 @@ export default function () {
     $(element).html(
       $(element)
         .html()
-        .replace(regex, (match) => `<a href="${discussionsUrl}?q=${match.slice(1)}" class="hasht" title="${tooltip}">${match}</a>`)
+        .replace(regex, (match) => ` <a href="${discussionsUrl}?q=${match.replace(/#/g,'').trim()}" class="hasht" title="${tooltip}">${match.trim()}</a>`)
     );
     setTimeout(() => {
       $(element)
