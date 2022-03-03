0ec969bbe81fc6a355ea220f26ef136841bc3cb8
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2022-03-03 17:06:10 -0500

    minimalism

--- js/src/forum/ReplaceHashTag.js
+++ js/src/forum/ReplaceHashTag.js
@@ -1,50 +1,15 @@
 import app from 'flarum/forum/app';
 
 export default function () {
-  if (app.forum.attribute('justoverclock-hashtag.regex')) {
-  const regex = new RegExp('' + app.forum.attribute('justoverclock-hashtag.regex'), 'gu');
-  const p = this.$('p');
-  const discussionsUrl = app.route('index');
-  const tooltip = app.translator.trans('justoverclock-hashtag.forum.post.hashtag_link_tooltip');
-
-  p.each((index, element) => {
-    $(element).html(
-      $(element)
-        .html()
-        .replace(regex, (match) => ` <a href="${discussionsUrl}?q=${match.replace(/#/g,'').trim()}" class="hasht" title="${tooltip}">${match.trim()}</a>`)
-    );
-    setTimeout(() => {
-      $(element)
-        .find('a.hasht')
-        .click((e) => {
-          if (e.ctrlKey || e.metaKey || e.which === 2) return;
-          e.preventDefault();
-          m.route.set(e.target.href);
-        });
-    }, 1);
-  });
-}
-if (app.forum.attribute('justoverclock-hashtag.regex') === '') {
   const regex = /(^|\s)#(\p{L}*\p{S}*\p{M}*)/gu;
   const p = this.$('p');
   const discussionsUrl = app.route('index');
-  const tooltip = app.translator.trans('justoverclock-hashtag.forum.post.hashtag_link_tooltip');
 
   p.each((index, element) => {
     $(element).html(
       $(element)
         .html()
-        .replace(regex, (match) => ` <a href="${discussionsUrl}?q=${match.replace(/#/g,'').trim()}" class="hasht" title="${tooltip}">${match.trim()}</a>`)
+        .replace(regex, (match) => ` <a href="${discussionsUrl}?q=${match.replace(/#/g,'').trim()}" class="hashtag">${match.trim()}</a>`)
     );
-    setTimeout(() => {
-      $(element)
-        .find('a.hasht')
-        .click((e) => {
-          if (e.ctrlKey || e.metaKey || e.which === 2) return;
-          e.preventDefault();
-          m.route.set(e.target.href);
-        });
-    }, 1);
   });
-}
 }
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,6 +3,6 @@ import { extend } from 'flarum/common/extend';
 import CommentPost from 'flarum/forum/components/CommentPost';
 import ReplaceHashTag from './ReplaceHashTag';
 
-app.initializers.add('justoverclock/flarum-ext-hashtag', () => {
+app.initializers.add('zerosonesfun/flarum-ext-hashtag', () => {
   extend(CommentPost.prototype, ['oncreate', 'onupdate'], ReplaceHashTag);
 });
